96807fc02bcea8314855d0695c8a58ede636a10c
Slatron <slatron@gmail.com>
2017-06-03 21:16:42 -0500

    advanced song filters

--- dev/js/angular_app/directives/song-viewer/songViewer.js
+++ dev/js/angular_app/directives/song-viewer/songViewer.js
@@ -21,39 +21,75 @@ directive('songViewer', function(
 
       // vm data
       vm.songsDB = firebaseFactory.followSongs();
+      var originalSongsDB = vm.songsDB;
       vm.titleFilter = '';
       vm.displaySong = undefined;
-      vm.obeyFilter = true;
+      vm.instrumentOptions = [
+        '----------',
+        'Bass',
+        'Banjo',
+        'Mandolin',
+        'Fiddle',
+        'Guitar',
+        'Electric',
+        'Harmonica'
+      ];
+      vm.instrument = vm.instrumentOptions[0];
+      vm.playerOptions = [
+        '----------',
+        'nate',
+        'mike',
+        'adam',
+        'carl'
+      ];
+      vm.player = vm.playerOptions[0];
 
       // vm functions
       vm.selectSong = selectSong;
       vm.randomSong = randomSong;
 
-      var setInitCount = $scope.$watchCollection(function() {
+      $scope.$watchCollection(function() {
         return vm.songsDB;
-      }, function (newVal) {
-        if (newVal && newVal.length) {
+      }, function(newVal) {
+        if (newVal && _.isArray(vm.songsDB)) {
           countSongs();
-          setInitCount();
         }
       });
 
       $scope.$watch(function() {
-        return vm.titleFilter;
-      }, function (newVal, oldVal) {
-        if (newVal !== undefined && newVal !== null && newVal !== oldVal) {
-          countSongs();
+        return vm.instrument;
+      }, function(newVal) {
+        if (newVal && newVal !== '----------') {
+          instrumentFilter();
+        } else if (newVal && newVal === '----------') {
+          vm.songsDB = originalSongsDB;
         }
       });
+
+      $scope.$watch(function() {
+        return vm.player;
+      }, function(newVal) {
+        if (newVal && newVal === '----------') {
+          vm.instrument = '----------';
+        } else if (newVal && newVal !== '----------' && vm.instrument !== '----------') {
+          instrumentFilter();
+        }
+      });
+
+      function instrumentFilter() {
+        vm.songsDB = originalSongsDB;
+        var key = vm.player;
+        vm.songsDB = $filter('filter')(vm.songsDB, function (song) {
+          return song[vm.player] === vm.instrument;
+        });
+      }
 
       function selectSong(song) {
         vm.displaySong = song;
       }
 
       function randomSong() {
-        var songList = vm.obeyFilter ?
-          $filter('filter')(vm.songsDB, vm.titleFilter) : vm.songsDB;
-        vm.displaySong = songList[_.random(songList.length - 1)];
+        vm.displaySong = vm.songsDB[_.random(vm.songsDB.length - 1)];
       }
 
       function countSongs() {
